939e76b490aa974518c5593f2907254e1e7f8d56
unknown <sebasocampo1414@gmail.com>
2026-06-24 22:35:44 -0500

    git2t

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel";
+import node from "@astrojs/node";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   output: "server",
-  adapter: vercel(),
+  adapter: node({
+    mode: "standalone",
+  }),
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
